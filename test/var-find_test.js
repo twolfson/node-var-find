2b0c8e746ce12bc467addfa958afbdb8512ba6c8
Todd Wolfson <todd@twolfson.com>
2013-09-27 20:27:22 -0700

    Finished up mark up with all splicing issues

--- test/var-find_test.js
+++ test/var-find_test.js
@@ -11,80 +11,99 @@ var testDir = __dirname + '/test_files',
 describe('var-find', function () {
   // Iterate over the test files (cases)
   testFiles.forEach(function (filename) {
-    before(function () {
-      // Load in the file contents
-      // [{var abc;}]
-      var testFile = fs.readFileSync(testDir + '/' + filename, 'utf8'),
-          testChars = testFile.split('');
+    describe('parsing ' + filename, function () {
+      before(function () {
+        // Load in the file contents
+        // [{var abc;}]
+        var testFile = fs.readFileSync(testDir + '/' + filename, 'utf8'),
+            testChars = testFile.split('');
 
-      // Extract the var groups
-      var groups = [];
-      while (true) {
-        // Find the next group start
-        // `[` + {var abc;}]
-        var groupStart = testChars.indexOf('[');
-
-        // If there was no match, stop
-        if (groupStart === -1) {
-          break;
-        }
-
-        // Remove the starting piece
-        // {var abc;}]
-        testChars.splice(groupStart, 1);
-
-        // Find the group end
-        // {var abc;} + `]`
-        var groupEnd = testChars.indexOf(']');
-        assert.notEqual(groupEnd, -1, 'Group starting at "' + groupStart + '" did not have an end.');
-
-        // Extract each var piece
-        var vars = [];
+        // Extract the var groups
+        var groups = [];
         while (true) {
-          // Find the next var start
-          // `{` + var abc;}]
-          var varStart = testChars.indexOf('{');
+          // Find the next group start
+          // `[` + {var abc;}]
+          var groupStart = testChars.indexOf('[');
 
-          // If there was no match or we have gone beyond the group end, stop
-          if (varStart === -1 || varStart > groupEnd) {
+          // If there was no match, stop
+          if (groupStart === -1) {
             break;
           }
 
           // Remove the starting piece
-          // var abc;}]
-          testChars.splice(varStart, 1);
+          // {var abc;}]
+          console.log('a', groupStart, testChars.join(''));
+          testChars.splice(groupStart, 1);
 
-          // Find the var end
-          // var abc; + `}` + ]
-          var varEnd = testChars.indexOf('}');
-          assert.notEqual(varEnd, -1, '`var` starting at "' + varStart + '" did not have an end.');
+          // Find the group end
+          // {var abc;} + `]`
+          var groupEnd = testChars.indexOf(']');
+          assert.notEqual(groupEnd, -1, 'Group starting at "' + groupStart + '" did not have an end.');
 
-          // Collect the var start and end
-          vars.push({
-            start: varStart,
-            end: varEnd
+          // Remove the group end
+          testChars.splice(groupEnd, 1);
+          console.log('b', groupEnd);
+
+          // Extract each var piece
+          var vars = [];
+          while (true) {
+            // Find the next var start
+            // `{` + var abc;}]
+            var varStart = testChars.indexOf('{');
+            console.log('yyy', varStart, groupEnd);
+
+            // If there was no match or we have gone beyond the group end, stop
+            if (varStart === -1 || varStart >= groupEnd) {
+              break;
+            }
+
+            // Remove the starting piece
+            // var abc;}]
+            testChars.splice(varStart, 1);
+            groupEnd -= 1;
+
+            // Find the var end
+            // var abc; + `}` + ]
+            var varEnd = testChars.indexOf('}');
+            assert.notEqual(varEnd, -1, '`var` starting at "' + varStart + '" did not have an end.');
+
+            // If we have gone beyond the group end, stop
+            if (varEnd >= groupEnd) {
+              break;
+            }
+
+            // Remove the var end
+            testChars.splice(varEnd, 1);
+            groupEnd -= 1;
+
+            // Collect the var start and end
+            vars.push({
+              start: varStart,
+              end: varEnd
+            });
+          }
+
+          // Save the vars for later
+          groups.push({
+            vars: vars,
+            start: groupStart,
+            end: groupEnd
           });
         }
 
-        // Save the vars for later
-        groups.push({
-          vars: vars,
-          start: groupStart,
-          end: groupEnd
-        });
-      }
+        // Collect chars
+        var content = testChars.join('');
 
-      // Collect chars
-      var content = testChars.join('');
+        // If there are any special chars remaining, complain
+        console.log('end', content);
+        var specialMatch = content.match(/\[|{|}|\]/);
+        assert.strictEqual(specialMatch, null);
 
-      // If there are any special chars remaining, complain
-      var specialMatch = content.match(/\[|{|}|\]/);
-      assert.strictEqual(specialMatch, null);
+      });
 
-      console.log(content);
+      it('contains expected groups of variables', function () {
+
+      });
     });
-  });
-  it('', function () {
-
   });
 });
